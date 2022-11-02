761d3711d0d89c5791b9128f889eb712d4bf7f91
CuteHand <2293150004@qq.com>
2022-11-02 19:36:06 +0800

    Update __init__.py

--- plot/__init__.py
+++ plot/__init__.py
@@ -5,11 +5,7 @@
 @Date    ：2022/9/29 20:22 
 '''
 
-from .chart_plot import (HA_kline,kline,stock_line,chart_line,chart_bar,
-                         chart_inv_bar,chart_pie,chart_heatmap,chart_heatmap_color,
-                         chart_map,cut_word,cloud_data,chart_wordcloud,
-                         chart_calendar,chart_box,)
+from .chart_plot import *
 
 
-from .data_plot import (line,scatter,pie,bar,box,violin,hist,hist_kde,treemap,
-                       plot_north_money, plot_stock_money,plot_ichimoku)
+from .data_plot import *
